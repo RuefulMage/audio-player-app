ab4c77c9d9251fed39d71a3f05793a5107b9abf6
Abdulkodir Burganov <abdulkodir@cucomm.com>
2025-04-07 01:05:14 +0300

    feat(api): move the audio list to a separate file and add more audios

---
+++ src/core/constants/audios.ts
@@ -0,0 +1,64 @@
+import {Audio} from "../models/audio";
+
+export const  MOCK_AUDIOS: Audio[] = [
+  {
+    id: 1,
+    name: 'Kangaroo MusiQue - The Neverwritten Role Playing Game',
+    url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
+    fileName: 'Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3'
+  },
+  {
+    id: 2,
+    name: 'Sevish',
+    url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3',
+    fileName: 'Sevish_-__nbsp_.mp3'
+  },
+  {
+    id: 3,
+    name: 'Ateapill',
+    url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/pyman_assets/ateapill.ogg',
+    fileName: 'ateapill.ogg'
+  },
+  {
+    id: 4,
+    name: 'Epoq Lepidoptera',
+    url: 'https://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg',
+    fileName: 'Epoq-Lepidoptera.ogg'
+  },
+  {
+    id: 5,
+    name: 'Evil Laugh',
+    url: 'https://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg',
+    fileName: 'Evillaugh.ogg'
+  },
+  {
+    id: 6,
+    name: 'week7 brrring',
+    url: 'https://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a',
+    fileName: 'week7-brrring.m4a'
+  },
+  {
+    id: 7,
+    name: 'Theme_01',
+    url: 'https://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/theme_01.mp3',
+    fileName: 'theme_01.mp3'
+  },
+  {
+    id: 8,
+    name: 'Paza Moduless',
+    url: 'https://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3',
+    fileName: 'paza-moduless.mp3'
+  },
+  {
+    id: 9,
+    name: 'Thrust',
+    url: 'https://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/thrust.mp3',
+    fileName: 'thrust.mp3'
+  },
+  {
+    id: 10,
+    name: 'Background Music',
+    url: 'https://codeskulptor-demos.commondatastorage.googleapis.com/descent/background%20music.mp3',
+    fileName: 'background music.mp3'
+  },
+];
--- src/core/services/InMemoryDataService.ts
+++ src/core/services/InMemoryDataService.ts
@@ -1,34 +1,10 @@
 import {Audio} from "../models/audio";
 import {InMemoryDbService} from "angular-in-memory-web-api";
+import {MOCK_AUDIOS} from "../constants/audios";
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const audios: Audio[] = [
-      {
-        id: 1,
-        name: 'Kangaroo MusiQue - The Neverwritten Role Playing Game',
-        url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
-        fileName: 'Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3'
-      },
-      {
-        id: 2,
-        name: 'Sevish',
-        url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3',
-        fileName: 'Sevish_-__nbsp_.mp3'
-      },
-      {
-        id: 3,
-        name: 'Ateapill',
-        url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/pyman_assets/ateapill.ogg',
-        fileName: 'ateapill.ogg'
-      },
-      {
-        id: 4,
-        name: 'Engine',
-        url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/fx/engine-1.ogg3',
-        fileName: 'engine-1.ogg3'
-      }
-    ];
+    const audios: Audio[] = structuredClone(MOCK_AUDIOS);
 
     return { audios };
   }
